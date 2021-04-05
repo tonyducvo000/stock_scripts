08b5439199da999929fe29462695e51abedd191d
tony <tonyducvo000@gmail.com>
2021-04-05 11:37:47 -0700

    initial commit for stock_cons.py

---
+++ stock_cons.py
@@ -0,0 +1,58 @@
+#Electric Vehicles
+class ev:
+    fisker = 'FSR'  # Fisker
+    kandi = 'KNDI'
+    li = 'LI'  # Li Auto
+    nio = 'NIO'
+    nikola = 'NKLA'
+    ride = 'RIDE' #Lordstown Motors
+    solo = 'SOLO'
+    tesla = 'TSLA'
+    workhorse = 'WKHS'
+    xpeng = 'XPEV'
+
+#Real Estate Investment Trusts
+class reits:
+    lamar = 'LAMR'
+    outfront = 'OUT'
+    aimco = 'AIV'
+    avalon = 'AVB'
+    camden = 'CPT'
+    equity = 'EQR'
+    essex = 'ESS'
+    midAmerica = 'MAA'
+    UDRinc = 'UDR'
+    coresite = 'COR'
+
+class green_energy:
+    beam = 'BEEM'
+    gevo = 'GEVO'
+    litETF = 'LIT'
+    litAmerica = 'LAC'
+    plug = 'PLUG'
+
+class aero_space:
+    aerojet = 'AJRD' #Aerojet Rocketdyne Holdings
+    boeing = 'BA'
+    echoStar = 'SATS'
+    loralSpace = 'LORL'
+    iridium = 'IRDM'
+    lockheed = 'LMT'
+    maxar = 'MAXR'
+    moon = 'MOON' #moonshot ETF
+    northrop = 'NOC'
+    raytheon = 'RTX'
+    virginGalactic = 'SPCE'
+
+class banks:
+    bankOfAmerica = 'BAC' #Bank of America
+    wellsFargo = 'WFC'
+    jpmorgan = 'JPM'
+    usbancorp = 'USB'
+    citigroup = 'C'
+    goldman = 'GS'
+
+
+
+
+
